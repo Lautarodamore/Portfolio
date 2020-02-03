6dd92401e5ec066a358de8104fb0c85cc45acc46
Lautaro <lauti.damore@gmail.com>
2020-02-03 18:53:27 -0300

    First commit

---
+++ layouts/default.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-app dark>
+    <v-toolbar color="#303030" :clipped-left="clipped" app>
+      <v-toolbar-title v-text="title" class="pri" />
+      <v-spacer />
+      <a href class="link-bar"> <span class="span-bar">1.</span> About </a>
+      <a href class="link-bar"> <span class="span-bar">2.</span> Experience </a>
+      <a href class="link-bar"> <span class="span-bar">3.</span> My work </a>
+      <a href class="link-bar"> <span class="span-bar">4.</span> Contact </a>
+      <v-btn class="btn-bar">Resume</v-btn>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2019</span>
+      <v-icon>linkedin</v-icon>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      fixed: false,
+      title: "LD"
+    };
+  }
+};
+</script>
+
+<style>
+.pri {
+  color: #00ed81;
+  font-weight: 300 !important;
+}
+
+.link-bar {
+  font-size: 15px;
+  padding: 12px 15px;
+  text-decoration: none;
+  color: white;
+}
+
+.link-bar:hover {
+  color: #00ed81;
+}
+
+.span-bar {
+  margin-right: 4px;
+  color: #00ed81;
+}
+
+.btn-bar {
+  color: #00ed81 !important;
+  font-size: 15px;
+  background-color: rgba(0, 0, 0, 0.1);
+  line-height: 1;
+  cursor: pointer;
+  margin-left: 10px;
+  font-size: 13px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #00ed81;
+  border-radius: 3px;
+  padding: 0.75rem 1rem;
+  text-decoration: none;
+  text-transform: none !important;
+  transition: all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1) 0s;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="pd-section">
+        <h3 data-aos="fade-right" class="saludo">Hi, my name is</h3>
+        <h1 data-aos="fade-right" class="nombre-obj">Lautaro Damore.</h1>
+        <h1
+          data-aos="fade-right"
+          class="nombre-obj"
+          style="color: rgb(153, 159, 176);"
+        >
+          I create websites.
+        </h1>
+        <p data-aos="fade-right" class="descripcion">
+          I'm a student of software engineer based in Argentina, Buenos Aires
+          specializing in building (and occasionally designing) exceptional,
+          high-quality websites and applications.
+        </p>
+
+        <v-btn data-aos="fade-right" class="btn-mail"
+          ><a
+            style="text-decoration: none; color: #00ed81;"
+            href="mailto:lauti.damore@gmail.com"
+            >Send my a mail</a
+          ></v-btn
+        >
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+
+export default {
+  head() {
+    return {
+      title: "Lautaro Damore | Full Stack Dev",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Lautaro Damore Full Stack Developer Portfolio "
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.pd-section {
+  padding: 150px 0px;
+}
+
+.saludo {
+  color: #00ed81;
+  font-size: 18px;
+  font-weight: 400;
+}
+
+.nombre-obj {
+  font-size: 70px;
+}
+
+.descripcion {
+  color: rgb(136, 146, 176);
+  line-height: 1.3;
+  font-size: 20px;
+  text-align: justify;
+}
+
+.btn-mail {
+  color: #00ed81;
+  background-color: rgba(0, 0, 0, 0.1);
+  font-size: 14px;
+  cursor: pointer;
+  margin-top: 50px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #00ed81;
+  border-image: initial;
+  border-radius: 3px;
+  height: 45px;
+  margin-left: 0;
+  text-decoration: none;
+  transition: all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1) 0s;
+}
+
+/* Media */
+@media (max-width: 600px) {
+  .descripcion {
+    font-size: 17px;
+  }
+}
+
+@media (min-width: 150px) {
+  .nombre-obj {
+    font-size: 40px;
+  }
+}
+@media (min-width: 600px) {
+  .nombre-obj {
+    font-size: 60px;
+  }
+}
+@media (min-width: 900px) {
+  .nombre-obj {
+    font-size: 70px;
+  }
+}
+</style>
